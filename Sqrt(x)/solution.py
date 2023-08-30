e6e0f99aa12d8e508baf68e82a0d827cd6d77af2
= <66052642+teyaligth@users.noreply.github.com>
2023-08-31 00:04:51 +0300

    updated sqrt(x)

--- Sqrt(x)/solution.py
+++ Sqrt(x)/solution.py
@@ -1,19 +1,17 @@
-
 class Solution:
-    def mySqrt(self, x: int) -> int:    
+    def mySqrt(self, x: int) -> int:
         if x == 0:
             return 0
-        
-        start=1
-        end=x
-        
+
+        start = 1
+        end = x
+
         while start < end - 1:
             median = start + int((end - start) / 2)
-            
+
             if median * median > x:
                 end = median
             else:
                 start = median
-        
+
         return start
-    
